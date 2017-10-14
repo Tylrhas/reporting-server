bcb0d48008bf682e98c0062a3aab73cc05e31f80
tylrhas <tylerhasenoehrl@Tylers-iMac.local>
2017-10-13 19:12:03 -0700

    added filters for the wis kpi query

--- api/models/wis_kpimodel.js
+++ api/models/wis_kpimodel.js
@@ -16,31 +16,73 @@ const client = new Client({
 
 // use of models
 exports.getCurrentQuarter = function (req, res) {
-  //create object out of req and res 
-  obj = {
-    'res': res,
-    'req':req
+  console.log(req.params.name)
+  if (req.params.name){
+    var query =  createQuery(req)
+
+    console.log(query)
+    //name was specified
+    getKpiData(req, res, query)
   }
-  getKpiData(obj)
+  else{
+    res.json("Please Select a WIS Name")
+  }
 }
 
-function getKpiData(obj){
+function getKpiData(req, res, query){
   client.connect((err) => {
     if (err) {
       console.error('connection error', err.stack)
     } else {
       console.log('connected')
-      client.query('SELECT * FROM lp_tasks',(err, querydata) => {
+      client.query(query,(err, querydata) => {
         if (err) throw err
         //add query data to the object 
-        obj.querydata = querydata.rows[0]
-        formatData(obj)
+        res.json(formatData(querydata.rows[0]))
         client.end()
       })
     }
   })
 }
+
 function formatData(obj){
   console.log(obj)
-  obj.res.json(obj.querydata);
+  return obj
+}
+function createQuery(req){
+
+  var daterange  = getQurterDateRange(req);
+  var query = {
+    // give the query a unique name
+    name: 'fetch-wisKPI',
+    text: 'select * from lp_tasks where owners = $1::text AND datedone BETWEEN $2::date AND $3::date',
+    values: [req.params.name, daterange.begining , daterange.ending]
+  }
+  return query
+}
+
+function getQurterDateRange(req){
+  var quarter = req.query.quarter
+  var year = req.query.year 
+  var dates =  {
+    "begining":'',
+    "ending":''
+}
+  if(quarter == 'q1'){
+    dates.begining = year + '-01-01'
+    dates.ending = year + '-03-31'
+  }
+  else if(quarter == 'q2'){
+    dates.begining = year + '-04-01'
+    dates.ending = year + '-06-30'
+  }
+  else if (quarter = 'q3'){
+    dates.begining = year + '-07-01'
+    dates.ending = year + '-09-30'
+  }
+  else{
+    dates.begining = year + '-10-01'
+    dates.ending = year + '-12-31'
+  } 
+  return dates
 }
--- api/routes/wis_kpi.js
+++ api/routes/wis_kpi.js
@@ -5,5 +5,7 @@ var kpi_controller = require('../controllers/wis_kpiController.js');
 
 router.get('/kpi/wis', kpi_controller.quarterkpi);
 
+router.get('/kpi/wis/:name', kpi_controller.quarterkpi);
+
 //router.get('/locations/launched/:vertical', location_controller.getLocationsLaunchedVertical);
 module.exports = router;
