f76387bd6d0357fd47d57b98259c99ba3f5f7fc3
Tyler Hasenoehrl <tylerhasenoehrl@Tylers-MacBook-Pro.local>
2017-11-12 17:08:03 -0800

    update readme

---
+++ README.md
@@ -0,0 +1,9 @@
+A Node app that Displays Metrics and data for G5 Pro Serv
+
+This app uses: Node.js + Bootstrap + Chart.js
+
+#Todo  
+Make the filters work on the reports page
+Update Jobs to use request module to make authenticated http requests to job endpoint
+Style Footer
+be able to download CSV of the current report
--- app/routes/pages.js
+++ app/routes/pages.js
@@ -2,4 +2,8 @@ module.exports = function (app, passport) {
     app.get('/reports/pmweightedprojects', function (req, res) {
         res.render('pages/pmweight', { user: req.user });
     });
+
+    app.get('/reports/monthlygoals', function (req, res) {
+        res.render('pages/monthlygoals', { user: req.user });
+    });
 }
