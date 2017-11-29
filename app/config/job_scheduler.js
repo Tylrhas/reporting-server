ad7c78ec2c75d28053ffc3ced4e0b0f85689089a
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2017-11-29 11:23:36 -0800

    update jobs

--- app/config/job_scheduler.js
+++ app/config/job_scheduler.js
@@ -10,7 +10,6 @@ lpLBS = require('../controllers/apifunctions/lp_lbs');
 LPTime = require('../controllers/jobs/lp_time_logged');
 
 //SCHEDULED JOBS
-
 schedule.scheduleJob('45 15 * * 1-5', function(){
 	console.log('Updating Client Time');
   clientTime.logClientTimeJob();
@@ -20,16 +19,16 @@ schedule.scheduleJob('* 21 * * *', function(){
   console.log('Updating LP Projects');
   lpProjects.updateProjects();
 });
-
+/*
 schedule.scheduleJob('30 21 * * *', function(){
   console.log('Updating LP LBS');
   lpLBS.update();
 });
 
-// schedule.scheduleJob('* 22 * * *', function(){
-//   console.log('Updating LP Tasks');
-//   lpTasks.updateLpTasksTableJob()
-// });
+schedule.scheduleJob('* 22 * * *', function(){
+  console.log('Updating LP Tasks');
+  lpTasks.updateLpTasksTableJob()
+});
 
 schedule.scheduleJob('30 22 * * *', function(){
   console.log('Updating QC Scores');
@@ -40,3 +39,4 @@ schedule.scheduleJob('0 * * * *', function(){
   console.log('Updating LP Times');
   LPTime.update();
 });
+*/
