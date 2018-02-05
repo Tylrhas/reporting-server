9c8923b143fc43f5743703e011d2b937898683da
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-02-05 10:48:27 -0800

    remove unneeded packages

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,4 @@
 "use strict";
-
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "development";
-var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(process.env.DATABASE_URL);
 var db        = {};
 
