84914ef692466d739813030b79759125b7c52604
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-02-05 12:48:59 -0800

    change index page

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 
-app.get('/', function (req, res) {
-    res.render('pages/index', { user : req.user} );
+// app.get('/', function (req, res) {
+//     res.render('pages/index', { user : req.user} );
 
-});
+// });
 
 //Models
 var models = require("./app/models");
