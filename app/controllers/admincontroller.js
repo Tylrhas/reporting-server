00206699b4236e50fc784829bade9cb006476cf8
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-02-05 10:46:53 -0800

    update to use config var

--- app/controllers/admincontroller.js
+++ app/controllers/admincontroller.js
@@ -3,7 +3,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config);
+var sequelize = new Sequelize(process.env.DATABASE_URL);
 var exports = module.exports = {}
 
 exports.jobs = function (req, res) {
