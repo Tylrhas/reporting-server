632a43231b5a3825d2531d29b032344fa3b31866
Tyler Hasenoehrl <tylrhas@gmail.com>
2018-04-24 18:40:59 -0700

    add create and delete webhooks

--- app/routes/lp_webhooks.js
+++ app/routes/lp_webhooks.js
@@ -6,7 +6,6 @@ module.exports = function (app, passport) {
   app.post('/webhooks/tasks', function (req, res) {
     // LP Post to the webhood to update the task
     res.sendStatus(200)
-    console.log(req.body)
     if (req.body.change_type === 'update') {
       //if change_type is update then update the record
       db.lp_task.update({
@@ -21,6 +20,26 @@ module.exports = function (app, passport) {
           id: req.body.id
         }
       })
+    }
+    else if(req.body.change_type === 'create'){
+      // add this task to the database
+      db.lp_task.create({
+        id: req.body.id,
+        e_start: req.body.expected_start,
+        e_finish: req.body.expected_finish, 
+        deadline: req.body.promise_by,
+        hrs_logged: req.body.hours_logged,
+        date_done: req.body.done_on,
+        hrs_remaning: req.body.high_effort_remaining
+      })
+      // trigger update of the other fields that do not come in webhooks like inherited tags
+    }
+    else if(req.body.change_type === 'delete'){
+      // potentially delete this task from LP
+      // { id: 45204698,
+      //   change_type: 'delete',
+      //   type: 'Task',
+      //   space_id: 158330 }
     }
   });
 }
