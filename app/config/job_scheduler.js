f0109040d49c91e8dc84fcd1d3729317e551efd2
Tyler Hasenoehrl <tylrhas@gmail.com>
2018-05-13 15:31:35 -0700

    remove old files

--- app/config/job_scheduler.js
+++ app/config/job_scheduler.js
@@ -8,17 +8,17 @@ lpLBS = require('../controllers/api/lp_lbs');
 
 
 // SCHEDULED JOBS
-// schedule.scheduleJob('45 15 * * 1-5', function(){
-// 	console.log('Updating Client Time');
-//   clientTime.logClientTimeJob();
-// });
+schedule.scheduleJob('45 15 * * 1-5', function(){
+	console.log('Updating Client Time');
+  clientTime.logClientTimeJob();
+});
 
-// schedule.scheduleJob('0 * * * *', function(){
-//   console.log('Updating LP LBS');
-//   lpLBS.update();
-// });
+schedule.scheduleJob('0 * * * *', function(){
+  console.log('Updating LP LBS');
+  lpLBS.update();
+});
 
-// schedule.scheduleJob('15 * * * *', function(){
-//   console.log('Updating Project Priorities');
-//   lpProjects.updatePriority(null,null);
-// });
+schedule.scheduleJob('15 * * * *', function(){
+  console.log('Updating Project Priorities');
+  lpProjects.updatePriority(null,null);
+});
