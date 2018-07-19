fd32d244e02444906431752a6c0d67ad10bbf75f
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-07-19 08:48:58 -0700

    move check parent out of try

--- app/routes/lp_webhooks.js
+++ app/routes/lp_webhooks.js
@@ -153,8 +153,6 @@ async function checkParent (body, subFolders) {
           }
           try {
             let parentBody = JSON.parse(body)
-            // create the project if it is missing
-            return checkParent(parentBody, subFolders)
           } catch (e) {
             //  post error to slack
             request({
@@ -167,7 +165,9 @@ async function checkParent (body, subFolders) {
               console.log(body);
             })
           }
-          if (parentBody !== null) {
+          // create the project if it is missing
+          return checkParent(parentBody, subFolders)
+          if (parentBody != null) {
             // create the project if it is missing
             return checkParent(parentBody, subFolders)
           }
