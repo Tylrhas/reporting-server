94190bff240dc67501ccec6da7bb294d851a55d0
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2018-10-19 15:51:53 -0700

    add archived projects job

--- app/config/job_scheduler.js
+++ app/config/job_scheduler.js
@@ -29,6 +29,11 @@ schedule.scheduleJob('0 * * * *', function() {
   console.log('updating LP data')
   apiController.updateProjects(null,null)
 })
+// update LP data every hour
+schedule.scheduleJob('30 * * * *', function() {
+ console.log('Updating Archived projects')
+ apiController.updateArchivedProjects(null,null)
+})
 
 // match LBS once a day 
 schedule.scheduleJob('0 1 * * *', function() {
