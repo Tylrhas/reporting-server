c964bd9e46059d18fe0ec6c1ef595a3369f1b97e
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2018-11-01 22:26:30 -0700

    update data pull from LP

--- app/controllers/apicontroller.js
+++ app/controllers/apicontroller.js
@@ -23,6 +23,42 @@ exports.updatelpLbsapi = function (req, res) {
  lp_lbs.updateapi(req, res);
 }
 
+exports.lbsAPIUpdate = async function (req, res) {
+ var locations
+ start_date = null
+ if (req && req.body.start_date) {
+  // check if there is a start date
+   start_date = req.body.start_date
+ }
+ locations = await lbs.getAnalyticsReport(start_date)
+ locations = locations.rows
+ for (let i = 0 ; i < locations.length; i++) {
+  // update each lbs with the new info
+  console.log(locations[i])
+  // parse out the location ID
+  let splitName = locations[i].name.split(/\s(.+)/, 2)
+   let LBSId = splitName[0]
+   let locationName = splitName[1]
+  // create the update object
+  let update = {
+   task_id: locations[i]["key"],
+   location_name: locationName,
+   project_id: locations[i]["project_id"],
+   stage: locations[i]["pick_list_custom_field:102670"],
+   original_estimated_go_live: locations[i]["date_custom_field:151494"],
+   estimated_go_live: locations[i]["date_custom_field:147376"],
+   actual_go_live: locations[i]["date_custom_field:151495"],
+   website_launch_date: locations[i]["date_custom_field:151496"],
+   start_date: locations[i]["date_custom_field:151496"],
+   project_lost_date: locations[i]["date_custom_field:151564"],
+  }
+  await lbs.update({id: LBSId}, update)
+ }
+// get the updates from the API
+
+// push the updates to the database
+}
+
 exports.findLBSProjects = function (req, res) {
  backfill.findMissingLBSProjects(req, res);
 }
