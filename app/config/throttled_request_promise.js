34763fb59f7dccad8085b57dfb1f91bc5e3d5fc0
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2018-12-18 13:55:50 -0800

    update configs

--- app/config/throttled_request_promise.js
+++ app/config/throttled_request_promise.js
@@ -1,8 +1,15 @@
 var throttledRequest = require('./throttled_request')
 var objectConstructor = {}.constructor
-module.exports = {
- promise
-}
+const request = require("request"),
+ throttledRequest = require('throttled-request')(request);
+
+ 
+//This will throttle the requests so no more than 15 are made every 15 seconds 
+throttledRequest.configure({
+ requests: 12,
+ milliseconds: 15000
+})
+
 function promise(args) {
  return new Promise(function (resolve, reject) {
   throttledRequest(args, function (error, response, body) {
@@ -21,4 +28,8 @@ function promise(args) {
    }
   })
  })
+}
+
+module.exports = {
+ promise
 }
