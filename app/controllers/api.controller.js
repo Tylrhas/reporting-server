1bac4faa40b9d3eddf99776f3e475568f91a804c
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2019-01-03 13:02:54 -0800

    add project controllers

--- app/controllers/api.controller.js
+++ app/controllers/api.controller.js
@@ -1,9 +1,26 @@
 var db = require('../models')
 module.exports = {
-  getAllProjects
+  getAllProjects,
+  getProject
 }
 
-async function getAllProjects (req, res) {
+async function getAllProjects(req, res) {
   let allProjects = await db.lp_project.findAll()
   res.json(allProjects)
+}
+
+async function getProject(req, res) {
+  try {
+    var results = await db.treeitem.find({
+      where: { id: req.params.project_id },
+      include: {
+        model: db.treeitem,
+        as: 'descendents',
+        hierarchy: true
+      }
+    })
+  } catch (error) {
+    results = error
+  }
+  res.json(results)
 }
