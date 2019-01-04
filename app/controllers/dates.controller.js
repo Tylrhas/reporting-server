14ba12e93e411c279611960a16a630615cf6d7e4
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2019-01-04 14:00:59 -0800

    add return null if input null

--- app/controllers/dates.controller.js
+++ app/controllers/dates.controller.js
@@ -1,9 +1,21 @@
 var moment = require('moment')
 var momentTz = require('moment-timezone')
 
-function pst_to_utc (date) {
-  // strip any timezone information
-  date = moment(date)
+function pst_to_utc(date) {
+  if (date !== null) {
+    // strip any timezone information
+    date = moment(date).tz('America/Los_Angeles')
+    // add in PST
+    date = momentTz(date).format()
+    // convert to UTC
+    date = moment(date).utc().format()
+    if (date == 'Invalid date') {
+      date = null
+    }
+  }
+  return date
+}
+function utc_to_pst(date) {
   // add in PST
   date = momentTz(date, 'America/Los_Angeles').format()
   // convert to UTC
@@ -13,7 +25,19 @@ function pst_to_utc (date) {
   }
   return date
 }
-function addPST (date) {
+function utc_to_pst_no_time(date) {
+  if (date != null) {
+  // convert to pst and remove hours 
+  date = moment(date).tz('America/Los_Angeles').format('MM-DD-YYYY')
+  if (date == 'Invalid date') {
+    date = null
+  }
+  return date
+} else {
+  return null
+}
+}
+function addPST(date) {
   return momentTz(date, 'America/Los_Angeles').format()
 }
 function now() {
@@ -25,20 +49,22 @@ function today() {
 function currentMonth() {
   return moment().format('MM')
 }
-function currentDay () {
+function currentDay() {
   return moment().format('DD')
 }
-function currentYear () {
+function currentYear() {
   return moment().format('YYYY')
 }
-function firstDay (month, year) {
+function firstDay(month, year) {
   return moment().month(--month).year(year).startOf('month').format('MM-DD-YYYY HH:mm:ss')
 }
-function lastDay (month, year) {
+function lastDay(month, year) {
   return moment().month(--month).year(year).endOf('month').format('MM-DD-YYYY HH:mm:ss')
 }
 module.exports = {
   pst_to_utc,
+  utc_to_pst,
+  utc_to_pst_no_time,
   moment,
   momentTz,
   now,
@@ -48,5 +74,5 @@ module.exports = {
   currentYear,
   firstDay,
   lastDay,
-  addPST
+  addPST,
 }
