fd12992cd26070e598ddbdaa2562ba1dc5c993e3
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2019-01-07 13:58:55 -0800

    add catch for no object body in response

--- app/config/throttled_request_promise.js
+++ app/config/throttled_request_promise.js
@@ -1,34 +1,43 @@
 var objectConstructor = {}.constructor
 const request = require("request"),
- throttledRequest = require('throttled-request')(request);
+  throttledRequest = require('throttled-request')(request);
 
- 
+
 //This will throttle the requests so no more than 12 are made every 15 seconds 
 throttledRequest.configure({
- requests: 5,
- milliseconds: 15000
+  requests: 5,
+  milliseconds: 15000
 })
 
 function promise(args) {
- return new Promise(function (resolve, reject) {
-  throttledRequest(args, function (error, response, body) {
-   if (error) {
-    error = {
-     error, response
-    }
-    reject(error)
-   }
-   else {
-    if (body.constructor === objectConstructor) {
-     resolve(body)
-    } else {
-     resolve(JSON.parse(body))
-    }
-   }
+  return new Promise(function (resolve, reject) {
+    throttledRequest(args, function (error, response, body) {
+      if (error) {
+        error = {
+          error, response
+        }
+        reject(error)
+      }
+      else {
+        if (body.constructor === objectConstructor) {
+          resolve(body)
+        } else {
+          try {
+            body = JSON.parse(body)
+            resolve(body)
+          } catch (error) {
+            if (error.__proto__.name === 'SyntaxError') {
+              resolve(body)
+            } else {
+              console.error(error)
+            }
+          }
+        }
+      }
+    })
   })
- })
 }
 
 module.exports = {
- promise
+  promise
 }
