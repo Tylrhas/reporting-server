c5216580d63f8b680d56d48472717d758752e0a5
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2019-02-05 13:28:58 -0800

    add null vars

--- app/config/job_scheduler.js
+++ app/config/job_scheduler.js
@@ -7,18 +7,18 @@ if (process.env.production =="true") {
 // update LP data every hour
 schedule.scheduleJob('0 * * * *', function() {
   console.log('updating Active Projects')
-  jobController.updateActiveProjects(null)
+  jobController.updateActiveProjects(null, null)
 })
 // update LP data every hour
 schedule.scheduleJob('30 * * * *', function() {
  console.log('Updating Archived projects')
- jobController.updateArchiveProjects(null)
+ jobController.updateArchiveProjects(null, null)
 })
 
 // match LBS once a day 
 schedule.scheduleJob('0 1 * * *', function() {
   console.log('updating LP data')
-  lsbController.match(null)
+  lsbController.match(null, null)
 })
 
 // match LBS once a day 
